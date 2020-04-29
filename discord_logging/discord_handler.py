67709f84e5737ff094a22fc8c482801cdcf90c33
grarich <54613530+grarich123@users.noreply.github.com>
2020-04-29 10:18:59 +0900

    Update discord_handler.py

--- discord_logging/discord_handler.py
+++ discord_logging/discord_handler.py
@@ -3,7 +3,6 @@ import aiohttp
 import asyncio
 import logging
 from logging import Handler
-import urllib.parse
 
 class AioHTTPHandler(Handler):
 
@@ -25,7 +24,13 @@ class AioHTTPHandler(Handler):
             data = self.mapLogRecord(record)
             headers["Content-type"] = "application/x-www-form-urlencoded"
             #can't do anything with the result
-            async with aiohttp.request("POST", url, data={content:f"{msg}"}, headers=headers):
-                pass
+            if len(msg) > 1900:
+              msg_list = [msg[i: i+1900] for i in range(0, len(msg), 1900)]
+              for i in msg_list:
+                async with aiohttp.request("POST", url, data={content:f"```{i}```"}, headers=headers):
+                    pass
+            else:
+              async with aiohttp.request("POST", url, data={content:f"```{msg}```"}, headers=headers):
+                    pass
         except Exception:
             self.handleError(record)
