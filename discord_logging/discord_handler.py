27f656dd49270cf7665c6c9dc43ff60303356c53
grarich <54613530+grarich123@users.noreply.github.com>
2020-04-29 10:23:39 +0900

    Update discord_handler.py

--- discord_logging/discord_handler.py
+++ discord_logging/discord_handler.py
@@ -3,7 +3,7 @@ import asyncio
 import logging
 from logging import Handler
 
-class AioHTTPHandler(Handler):
+class Discrd_Handler(Handler):
 
     def __init__(self, url):
         logging.Handler.__init__(self)
